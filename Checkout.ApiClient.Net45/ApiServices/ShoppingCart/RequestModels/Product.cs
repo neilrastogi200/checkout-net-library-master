eeb180cefb3752e13424efbe3ca351c0331714b1
neil.rastogi <neil.rastogi@tungsten-network.com>
2017-01-19 23:23:28 +0000

    Add project files.

---
+++ Checkout.ApiClient.Net45/ApiServices/ShoppingCart/RequestModels/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkout.ApiServices.ShoppingCart.RequestModels
+{
+    public class Product
+    {
+        public int ProductId { set; get; }
+
+        public string Name { set; get; }
+
+        public int Quantity { set; get; }
+
+        public decimal Price { set; get; }
+    }
+}
---
+++ Checkout.ApiClient.Net45/ApiServices/ShoppingCart/ShoppingCartService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Checkout.ApiServices.Cards.RequestModels;
+using Checkout.ApiServices.Cards.ResponseModels;
+using Checkout.ApiServices.SharedModels;
+using Product = Checkout.ApiServices.ShoppingCart.RequestModels.Product;
+
+namespace Checkout.ApiServices.ShoppingCart
+{
+    public class ShoppingCartService
+    {
+        public HttpResponse<Product> CreateProduct(Product requestModel)
+        {
+            var createShoppingCartUri = string.Format(ApiUrls.ShoppingCart);
+            return new ApiHttpClient().PostRequest<Product>(createShoppingCartUri, AppSettings.SecretKey, requestModel);
+        }
+    }
+}
